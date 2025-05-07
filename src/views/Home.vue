a718138ed2ce2254e738229cdb5e8c1abc464447
Pablo Jara <pablojaramunoz@icloud.com>
2025-05-07 12:19:42 -0300

    add compo pagination, jokecard

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Chistes</h1>
+
+        <label>Ordenar por:</label>
+        <select v-model="sortKey">
+            <option value="id">ID</option>
+            <option value="type">Tipo</option>
+        </select>
+
+        <div>
+            <JokeCard/>
+        </div>
+
+        <Pagination />
+    </div>
+</template>
+
+<script setup>
+
+</script>
