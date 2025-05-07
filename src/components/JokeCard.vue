2b34b97f01cf8c304621f67fa01d0cf4899f7183
Pablo Jara <pablojaramunoz@icloud.com>
2025-05-07 12:35:47 -0300

    logic for rating

--- src/components/JokeCard.vue
+++ src/components/JokeCard.vue
@@ -3,11 +3,22 @@
         <p><strong>{{ joke.setup }}</strong></p>
         <p>{{ joke.punchline }}</p>
         <small>Tipo: {{ joke.type }}</small>
+
+        <div style="margin-top: 0.5rem;">
+            <Rating v-model="rating" />
+            <p>Tu calificación: {{ rating }}/5</p>
+        </div>
     </div>
 </template>
 
 <script setup>
+
+import Rating from './Rating.vue'
+import { ref } from 'vue'
+
 defineProps({
     joke: Object
 })
+
+const rating = ref(0)
 </script>
